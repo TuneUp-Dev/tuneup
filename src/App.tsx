24b2df488d45725f8dbe8b0ffd45eb89aa34b707
Srini-10 <srinivasanp1018@gmail.com>
2024-11-20 08:03:14 +0530

    Fools Bday

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Routes/Router";
-import BirthdayCelebration from "./BirthdayCelebration";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Router /> 
-      </BrowserRouter> */}
-
-      <BirthdayCelebration />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </>
   );
 }
