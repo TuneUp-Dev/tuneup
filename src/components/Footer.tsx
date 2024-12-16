007b04570a4eaf2bb604b25c2a190c147cd07646
sairam-jeeva <sairamjeeva18@gmail.com>
2024-12-16 23:49:35 +0530

    feat:Content-feed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-[#021734] pt-10 mt-10 md:mt-0">
       <div className="max-w-[86vw] md:max-w-[88vw] xl:max-w-[1200px] mx-auto grid grid-cols-2 md:grid-cols-3 gap-y-8 lg:gap-y-0 lg:flex justify-between py-8 md:py-12 lg:py-16 xl:py-20">
-        {/* Product Section */}
+      
         <div>
           <h4 className="font-medium text-white lg:mb-1.5 xl:mb-2 text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px]">
             Product
