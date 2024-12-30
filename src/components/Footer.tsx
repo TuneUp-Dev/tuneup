d91ee5858070f709487563a68299236d84e36a31
Srinivasan P <136215579+Srini-10@users.noreply.github.com>
2024-12-30 19:45:13 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -216,7 +216,7 @@ const Footer = () => {
               <div className="md:block flex flex-col items-center justify-center text-center md:text-left w-full md:w-[410px]">
                 <img className="w-12 cursor-pointer" src={Logo} alt="" />
                 <p className="text-slate-200 text-[11px] lg:text-[12px] xl:text-[13px] font-light mt-3">
-                  &copy; {new Date().getFullYear()}{" "}
+                  ©{new Date().getFullYear()}{" "}
                   <b className="font-medium text-white">TuneUp Technologies</b>,
                   Inc. All rights reserved.
                 </p>
